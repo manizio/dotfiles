7727b7b788ea2716f26b2f4a2932c357dec44036
manizio <manoel.oliveira.066@ufrn.edu.br>
2024-05-23 16:41:34 -0300

    update autopairs and telescope

--- nvim/lua/manizio/plugins/telescope.lua
+++ nvim/lua/manizio/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    'nvim-telescope/telescope.nvim', tag = '0.1.7',
     -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim',
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
